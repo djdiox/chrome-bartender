e1e49e236388810772f0b898492e61dc34831ed3
Markus <markus.wagner@zoi.de>
2021-12-17 04:18:36 +0100

    started new chrome extension

---
+++ src/background.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import chromeP from 'webext-polyfill-kinda';
+browser.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
+  console.log('Hello from the background', { request: request, sender: sender, sendResponse: sendResponse })
+  const response = await axios.get('chrome://system/');
+  console.log(response.data);
+  const res = await chromeP.management.getAll();
+  console.log(res);
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+      {{JSON.stringify(extensions)}}
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import chromeP from 'webext-polyfill-kinda';
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      extensions: []
+    }
+  },
+  async mounted() {
+    this.extensions = await chromeP.management.getAll()
+  }
+}
+</script>
