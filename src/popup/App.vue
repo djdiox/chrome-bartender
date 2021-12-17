1e4fcd3a7151ea6612e0c69a49197635b186b306
Markus <markus.wagner@zoi.de>
2021-12-17 04:39:23 +0100

    added readme

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <hello-world />
+  <div>
+    <!-- <hello-world /> -->
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: { HelloWorld }
+  // components: { HelloWorld }
 }
 </script>
 
